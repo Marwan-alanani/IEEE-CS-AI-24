785bce41d1b0071479330d7abdf1f097f83eae88
Marwan-alanani <marwan-walid1@hotmail.com>
2024-04-26 20:16:56 +0300

    task 9 done

--- Task_9/Problems/broadcast.py
+++ Task_9/Problems/broadcast.py
@@ -1,5 +1,9 @@
 import numpy as np
-n = int(input())
+n = input("Enter dimensions: ")
+while not n.isdigit():
+    print("dimensions must be an integer!")
+    n = input("Enter dimensions: ")
+n = int(n)
 arr = np.zeros((n,n),'int32')
 arr += np.arange(1,n+1)
 print(arr)
